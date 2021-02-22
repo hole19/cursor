4f88f874d4835c1a7297f980679d00b955b88e8d
André Andrade <andrepalasandrade@gmail.com>
2021-02-22 12:18:35 +0000

    load configuration on activerecord

--- lib/cursor.rb
+++ lib/cursor.rb
@@ -1,26 +1,26 @@
-module Cursor
-end
-
-# load Rails/Railtie
-begin
-  require 'rails'
-rescue LoadError
-  #do nothing
-end
-
-# load Cursor components
-require 'cursor/config'
-require 'cursor/models/page_scope_methods'
-require 'cursor/models/configuration_methods'
-
-if defined? ::Rails::Railtie
-  require 'cursor/railtie'
-  require 'cursor/engine'
-end
-
 require 'active_support/lazy_load_hooks'
 
 ActiveSupport.on_load :active_record do
+  module Cursor
+  end
+
+  # load Rails/Railtie
+  begin
+    require 'rails'
+  rescue LoadError
+    #do nothing
+  end
+
+  # load Cursor components
+  require 'cursor/config'
+  require 'cursor/models/page_scope_methods'
+  require 'cursor/models/configuration_methods'
+
+  if defined? ::Rails::Railtie
+    require 'cursor/railtie'
+    require 'cursor/engine'
+  end
+
   require 'cursor/models/active_record_extension'
   ::ActiveRecord::Base.send :include, Cursor::ActiveRecordExtension
 end
